bf008c5b87fbaf372183886c76e09c2397c415c4
Erdem Olcay <erdem@olcay.org>
2018-05-10 01:51:43 +0300

    If Duckduckgo proxy doesn't return HTTP/200, try again.

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+var reloadCount = 0;
+
 // Redirect imgur.com requests to Duckduckgo proxy
 chrome.webRequest.onBeforeRequest.addListener(function (details) {
     var redirectUrl = "https://proxy.duckduckgo.com/iu/?u=" + details.url;
@@ -9,6 +11,12 @@ chrome.webRequest.onBeforeRequest.addListener(function (details) {
 
 // Modify valid JS sources when imgur page loads from Duckduckgo proxy
 chrome.webRequest.onHeadersReceived.addListener(function (details) {
+    // If HTTP status is not OK, try again
+    if (details.frameId === 0 && details.statusCode !== 200 && reloadCount < 2) {
+        chrome.tabs.reload(details.tabId);
+        reloadCount++;
+    }
+
     $.each(details.responseHeaders, function (index, item) {
         if (item.name === "content-security-policy" && item.value.indexOf("script-src") >= 0) {
             details.responseHeaders[index].value = "script-src self http: https: about: 'unsafe-inline' 'unsafe-eval'";
