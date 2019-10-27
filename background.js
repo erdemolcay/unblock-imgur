d2e7c35fa675773b65c77c39aa50c8d0026f7567
fakirpic <mafiaecel@gmail.com>
2019-10-27 03:39:39 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,9 +1,6 @@
 chrome.webRequest.onBeforeRequest.addListener(function (details) {
-	if(details.url.indexOf('imgur.com')>-1)
-	{
     return {redirectUrl: details.url.replace("imgur.com", "imgur.icu")}
-	}
 }, {
-    urls: ["<all_urls>"],
+    urls: ["*://*.imgur.com/*"],
 	types : ["main_frame", "sub_frame", "stylesheet", "script", "image", "font", "object", "xmlhttprequest", "ping", "csp_report", "media", "websocket", "other"]
 }, ["blocking"]);
